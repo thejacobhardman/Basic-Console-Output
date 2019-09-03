4c84bb2f40a768a5bf580906dfb8ca696f285ef7
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-09-03 13:51:24 -0600

    add console statements for different assignments

--- Basic-Console-Output/Basic-Console-Output.cpp
+++ Basic-Console-Output/Basic-Console-Output.cpp
@@ -5,7 +5,45 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    //cout << "Generic McNobody\n123 Wallaby Way, Kangaroo City CO 12345\n(123) 456-7890\nSoftware Engineering Major.";
+
+	/*
+
+	int spaces = 6;
+	int count = 1;
+
+	for (int i = 1; i < 6; i++) {
+		cout << string(spaces, ' ') << string(count, '*') << endl;
+		spaces -= 1;
+		count += 2;
+	}
+
+	spaces = 3;
+	count = 7;
+
+	for (int i = 4; i > 0; i--) {
+		cout << string(spaces, ' ') << string(count, '*') << endl;
+		spaces += 1;
+		count -= 2;
+	}
+
+	*/
+
+	/*
+	
+	cout << "Data Type:				Data Size:" << endl;
+	for (int i = 0; i < 11; i++) {
+		cout << string(i, '-');
+	}
+	cout << endl;
+	cout << "char					" << sizeof(char) << endl;
+	cout << "int					" << sizeof(int) <<endl;
+	cout << "float					" << sizeof(float) << endl;
+	cout << "double					" << sizeof(double) <<endl;
+
+	*/
 }
